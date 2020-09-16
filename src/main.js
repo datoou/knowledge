00822796fc261bf24644896d1fd1ee26f1dbd2a1
mwj <214410781@qq.com>
2020-09-16 10:15:17 +0800

    mwj

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/config/rem'
+import {Message,Modal} from "ant-design-vue";
+Vue.use(Modal)
 
+Vue.prototype.$message = Message
 Vue.config.productionTip = false
+
+// if (process.env.NODE_ENV == 'development') require('@/api/mock')
 
 new Vue({
   router,
